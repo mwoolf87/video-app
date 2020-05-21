9466ce812811dc97eb594cc0d436115f33dad2f2
mwoolf87 <michael.j.woolf@gmail.com>
2020-05-21 18:11:19 -0400

    finished styling app, made some ui changes

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,9 @@ import VideoDetail from "./VideoDetail";
 const KEY = "AIzaSyAnvsH03lIRraG4ypKT5NL5TheJfl - jR80";
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
+  componentDidMount() {
+    this.onTermSubmit("coding");
+  }
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: {
@@ -18,7 +21,10 @@ class App extends React.Component {
       },
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
 
   onVideoSelect = (video) => {
@@ -29,11 +35,19 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect={this.onVideoSelect}
-          videos={this.state.videos}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
